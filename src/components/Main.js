0c0edf7d22985108ec86e95d633ccecae06d421c
Kevin Compton <kevincomptondev@gmail.com>
2023-04-01 12:24:22 -0400

    Added changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,40 @@
-import { useEffect, UseState } from "react"
+import { useEffect, useState } from "react"
 import { Routes, Route } from "react-router-dom"
 import Index from "../pages/Index"
 import Show from "../pages/Show"
 
+
+
 const Main = (props) => {
+    const [people, setPeople] = useState(null)
+    const URL = 'http://localhost:4000/people/'
+
+    const getPeople = async() => {
+        const response = await fetch(URL)
+        const data = await response.json()
+        setPeople(data.data)
+    }
+
+    const createPerson = async (person) => {
+        await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(person)
+        })
+        //Update List of peoiple in state
+        getPeople()
+    }
+
+    useEffect(() => {
+        getPeople()
+    }, [])
+
     return (
         <main>
             <Routes>
-                <Route path='/' element={ <Index /> } />
+                <Route path='/' element={ <Index people={people} createPerson={createPerson} /> } />
                 <Route path='/people/:id' element={ <Show /> } />
             </Routes>
         </main>
